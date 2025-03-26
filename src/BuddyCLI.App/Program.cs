cfe4c3d0ee5982426687790343337b9bc7631327
Łukasz Mastalerz <lukasz.mastalerz@buddy.works>
2025-03-26 12:03:32 +0100

    Init

---
+++ src/BuddyCLI.App/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BuddyCLI.App;
+
+public enum ExitCode
+{
+    Success = 0,
+}
+
+class App(string[] args)
+{
+    private ExitCode Process()
+    {
+        return ExitCode.Success;
+    }
+    
+    static int Main(string[] args)
+    {
+        var app = new App(args);
+        return (int)app.Process();
+    }
+}
